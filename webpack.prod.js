83098c9ca7ec0ecc24401363af3ff29761e90602
Yuliya Khilko <ykhilko@gmail.com>
2021-03-01 10:32:26 -0800

    copy to dist

--- webpack.prod.js
+++ webpack.prod.js
@@ -36,7 +36,7 @@ module.exports = merge(common, {
     }),
     new CopyPlugin({
       patterns: [
-        { from: path.resolve(__dirname, 'public'), to: path.resolve(__dirname, 'dist')},
+        { from: path.resolve(__dirname, 'public'), to: path.resolve(__dirname, 'dist/public')},
       ],
     })
   ],
